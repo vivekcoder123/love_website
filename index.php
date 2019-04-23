5590a566f9818efb8a43013b9c3ccfc53b78657f
vivekcoder123 <vivekrautela000@gmail.com>
2019-04-23 13:50:08 +0530

    updated

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+<?php 
+
+function getUserIP() {    
+  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
+}
+
+$ip=getUserIP();
+echo "User ip is ".$ip;
 
 
+?>
 <!DOCTYPE html>
 <html lang="zxx">
 
